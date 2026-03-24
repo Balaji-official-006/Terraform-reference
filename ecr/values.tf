cc38a002eee98aab9dbcfb0588c37c548d895706
Balaji-official-006 <balajibhagavan2003@gmail.com>
2026-03-24 13:38:57 +0530

    Update values.tf

--- ecr/values.tf
+++ ecr/values.tf
@@ -1,1 +1,34 @@
+# ==============================
+# ECR-values.tf
+# ==============================
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "repository_name" {
+  description = "ECR repository name"
+  type        = string
+  default     = "balaji-ecr-repository"
+}
+
+variable "image_tag_mutability" {
+  description = "Image tag mutability setting"
+  type        = string
+  default     = "MUTABLE"
+}
+
+variable "environment" {
+  description = "Environment tag"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Project tag name"
+  type        = string
+  default     = "terraform-ecr-project"
+}
 
